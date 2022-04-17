1f851be37433dfb950c68892c00c099af0f85e92
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-04-17 11:00:21 -0700

    fix comments and make it private

--- contracts/DosuInvite.sol
+++ contracts/DosuInvite.sol
@@ -8,8 +8,8 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
-  @title Dosu Invites NFT
-  @dev Acts as a key to use https://dosu.io
+ * @title Dosu Invites NFT
+ * @dev Acts as a key to use https://dosu.io
  */
 contract DosuInvites is ERC721, ERC721Enumerable, Ownable {
   // Uses
